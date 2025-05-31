4958e4f34eea946b958db1de190357376eb3a2db
Marcos-Aurelio12 <nicholas_carvalho@estudante.sesisenai.org.br>
2025-05-31 15:41:43 -0300

    b.java

---
+++ b.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class b{
+
+  public static void main(String[] args){
+
+    String nome = JOptionPane.showInputDialog("Insira seu nome:");
+    JOptionPane.showMessageDialog(null, nome);
+  }
+}
